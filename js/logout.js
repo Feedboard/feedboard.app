59b4c6b5e43c7323006169cc142a4bc8cefc1518
monzanifabio <renderbros@gmail.com>
2023-11-01 11:55:27 +0000

    Logout redirect to landing

--- js/logout.js
+++ js/logout.js
@@ -3,5 +3,5 @@ const logoutBtn = document.getElementById("logoutBtn");
 logoutBtn.addEventListener("click", async (event) => {
   event.preventDefault();
   const { error } = await client.auth.signOut();
-  window.location.replace("/login.html");
+  window.location.replace("/index.html");
 });
