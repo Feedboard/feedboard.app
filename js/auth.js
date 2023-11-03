c08d4b9c3c51c855d93f543f7d7e71183502d5c9
monzanifabio <renderbros@gmail.com>
2023-11-03 00:27:24 +0000

    Merged login signup into auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,26 @@
 const userEmail = document.getElementById("userEmail");
 const userPassword = document.getElementById("userPassword");
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+  registerBtn.disabled = true;
+  const { data, error } = await client.auth.signUp({
+    email: userEmail.value,
+    password: userPassword.value,
+  });
+  if (error) {
+    document.getElementById("signupHelper").innerHTML = error;
+    console.log(error);
+    registerBtn.disabled = false;
+  } else {
+    console.log(data);
+    window.location.replace("./success.html");
+  }
+});
+
 const loginBtn = document.getElementById("loginBtn");
+const googleAuthBtn = document.getElementById("googleAuthBtn");
 
 loginBtn.addEventListener("click", async (event) => {
   event.preventDefault();
@@ -37,4 +57,13 @@ loginBtn.addEventListener("click", async (event) => {
     console.log("not a valid email address.");
     loginBtn.disabled = false;
   }
+});
+
+googleAuthBtn.addEventListener("click", async function () {
+  const { data, error } = await client.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: "https://feedboard.app/dashboard.html",
+    },
+  });
 });
