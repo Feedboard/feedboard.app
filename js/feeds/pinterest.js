6a049b8b35eb8cb9e11151faf3c2274f827fcf20
monzanifabio <renderbros@gmail.com>
2023-11-09 22:30:33 +0000

    Added Pinterest script

---
+++ js/feeds/pinterest.js
@@ -0,0 +1,144 @@
+async function getPinterestAccount(username, id) {
+  console.log("Loading " + username);
+  const pinterestAccountUrl = "https://web-production-ba07.up.railway.app/https://pinterest.co.uk/" + username + "/feed.rss";
+  const feedPinterestAccount = document.getElementById("feed-pinterestAccount-" + id);
+
+  await fetch(pinterestAccountUrl, {
+    headers: {
+      "Access-Control-Allow-Origin": pinterestAccountUrl,
+      "Access-Control-Allow-Headers": "content-type",
+    },
+  })
+    .then((response) => response.text())
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((data) => {
+      const entries = data.querySelectorAll("item");
+      feedPinterestAccount.innerHTML = "";
+      let entry = "";
+      if (entries.length <= 0) {
+        console.log("not ok");
+        entry += `
+        <div class="alert alert-warning d-flex align-items-center border-0 rounded-0 p-2" role="alert">
+          <img class="me-2" src="./img/warning-diamond.svg" width="20" height="20" />
+          <div>
+            This Pinterest account is not public or doesn't seem to exist.
+          </div>
+        </div>
+              `;
+        feedPinterestAccount.innerHTML = entry;
+      }
+
+      entries.forEach((el) => {
+        let title = el.querySelector("title").textContent;
+        let description = el.querySelector("description").textContent;
+        let pubDate = el.querySelector("pubDate").textContent;
+        let link = el.querySelector("link").textContent;
+
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(description, "text/xml");
+
+        const documentRoot = xmlDoc.documentElement;
+        const cdataContent = documentRoot.textContent;
+
+        const htmlDoc = new DOMParser().parseFromString(description, "text/html");
+        const imgEl = htmlDoc.querySelector("img");
+        console.log(imgEl);
+
+        entry += `
+            <a href="${link}" class="list-group-item list-group-item-action" target="_blank">
+              ${imgEl ? `<img class="img-fluid rounded-3" src="${imgEl.getAttribute("src")}" />` : ""}
+              <p class="fw-semibold mb-2">${title}</p>
+              <p class="text-secondary small">${convertHnDate(pubDate)}</p>
+            </a>
+              `;
+      });
+      feedPinterestAccount.innerHTML = entry;
+    });
+}
+
+// Add new Pinterest account
+const addNewPinterestAccountBtn = document.getElementById("addNewPinterestAccount");
+const newPinterestAccountName = document.getElementById("newPinterestAccountName");
+
+newPinterestAccountName.addEventListener("input", function () {
+  checkInputFill(newPinterestAccountName, addNewPinterestAccountBtn);
+});
+
+addNewPinterestAccountBtn.addEventListener("click", async function () {
+  closeModal("newFeedModal");
+  addNewPinterestAccountBtn.disabled = true;
+
+  const { data, error } = await client
+    .from("feeds")
+    .insert([{ feed_name: "pinterest", feed_type: "pinterest-account", feed_options: newPinterestAccountName.value, user_id: user_id }])
+    .select();
+
+  if (data) {
+    showToast(newPinterestAccountName.value + " added to your feed");
+    const feedContainer = document.getElementById("feedContainer");
+    const sidebarContainer = document.getElementById("feedLogoContainer");
+    let feed = "";
+    let sidebar = "";
+
+    sidebar += `
+         <a id="sidebarLogo-${data[0].id}" href="#${data[0].id}" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="${data[0].feed_options}">
+         <img class="rounded-3 m-2" src="./img/logo-pinterest.svg" alt="" width="40" height="40" />
+         </a>
+        `;
+
+    feed += `
+        <div id="${data[0].id}" class="feed border-end">
+          <div class="feed-header d-flex flex-row justify-content-between bg-body-tertiary border-bottom">
+            <div class="d-flex align-items-center">
+              <img class="me-2" src="./img/logo-pinterest.svg" width="20" height="20" alt="" />
+              <p>${data[0].feed_options}</p>
+            </div>
+            <div class="btn-group">
+              <button type="button" class="btn bg-body-tertiary btn-sm p-0 rounded-1 border-0" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="./img/dots-three-vertical.svg" width="24" height="24" alt="" />
+              </button>
+              <ul class="dropdown-menu dropdown-menu-end">
+                <li onclick="getPinterestAccount('${data[0].feed_options}', ${data[0].id})"><button class="dropdown-item" type="button">Reload</button></li>
+                <li onclick="removePinterestAccount(${data[0].id})"><button class="dropdown-item" type="button">Remove</button></li>
+              </ul>
+            </div>
+          </div>
+          <div id="feed-pinterestAccount-${data[0].id}" class="list-group list-group-flush feed-body">
+            <div class="p-2 placeholder-glow">
+              <span class="placeholder placeholder-lg col-6 bg-secondary"></span>
+              <span class="placeholder col-7 bg-secondary"></span>
+              <span class="placeholder col-4 bg-secondary"></span>
+              <span class="placeholder placeholder-sm col-2 bg-secondary"></span>
+            </div>
+          </div>
+        </div>
+        `;
+    hideEmpty();
+    feedContainer.innerHTML += feed;
+    sidebarContainer.innerHTML += sidebar;
+    scrollToPos(data[0].id);
+    getPinterestAccount(data[0].feed_options, data[0].id);
+  }
+
+  newPinterestAccountName.value = "";
+
+  if (error) {
+    console.log(error);
+  }
+  initTooltip();
+});
+
+// Remove PInterest account
+async function removePinterestAccount(id) {
+  const { error } = await client.from("feeds").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Deleted");
+    showToast("Feed deleted");
+    let feedContainer = document.getElementById(id);
+    let sidebarLogo = document.getElementById("sidebarLogo-" + id);
+    feedContainer.remove();
+    sidebarLogo.remove();
+  }
+}
