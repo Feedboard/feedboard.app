35eddccc6953e26bc92e46451af4345226fefbc4
monzanifabio <renderbros@gmail.com>
2023-11-16 15:26:40 +0000

    Changelog events styling

--- js/changelog.js
+++ js/changelog.js
@@ -10,10 +10,13 @@ changelogBtn.addEventListener("click", function () {
       data.forEach(function (value) {
         details += `
         <div class="d-flex mb-4">
-        <span class="bg-body-secondary align-self-start me-2 mt-1 p-1 rounded-circle">
-        <img class="" src="./img/check-circle-dark.svg" width="28" height="28" alt="check icon" />
-        </span>
-        <div>
+          <div class="d-flex flex-column align-items-center">
+            <span class="bg-body-secondary align-self-start mt-1 p-1 rounded-circle">
+            <img class="" src="./img/check-circle-dark.svg" width="28" height="28" alt="check icon" />
+            </span>
+            <div class="vr align-self-center h-100 mt-2 bg-body-secondary"></div>
+          </div>
+        <div class="ms-2">
         <p class="fw-semibold">${value.title}</p>
         <p class="small text-secondary mb-2">${value.date}</p>
         <p class="text-secondary">${value.description}</p>
