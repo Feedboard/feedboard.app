27d8b0b279a5e099a6f765df26413879dd3e2fcc
monzanifabio <renderbros@gmail.com>
2024-01-04 20:15:49 +0000

    Add temp email to temp db

--- js/set-temp-email.js
+++ js/set-temp-email.js
@@ -1,6 +1,6 @@
 const createBoardBtn = document.getElementById("createBoardBtn");
 
-createBoardBtn.addEventListener("click", function () {
+createBoardBtn.addEventListener("click", async function () {
   temp_email = document.getElementById("landingEmailField").value;
   sessionStorage.setItem("temp_email", temp_email);
 });
