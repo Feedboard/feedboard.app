9b8f4ddd44a22fef9f122ab902c84470b5ae9bf4
monzanifabio <renderbros@gmail.com>
2024-05-19 18:02:17 +0100

    Fixed session function execution

--- js/weeklyNewsletter.js
+++ js/weeklyNewsletter.js
@@ -1,7 +1,9 @@
 const weeklyEmailSwitch = document.getElementById("weeklyEmailSwitch");
 
-checkSession();
-getEmailState(user_id);
+document.addEventListener("DOMContentLoaded", async function () {
+  await checkSession();
+  getEmailState(user_id);
+});
 
 async function getEmailState(user_id) {
   const { data, error } = await client.from("settings").select("*").eq("user_id", user_id);
