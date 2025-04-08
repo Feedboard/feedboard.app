028d5f7337dc1a65cc635c7fe0c764d04b2bb4aa
Fabio Monzani <renderbros@gmail.com>
2025-04-08 21:58:43 +0100

    Improvements to readmode

--- js/feeds/readmode.js
+++ js/feeds/readmode.js
@@ -1,12 +1,6 @@
 const readmodeBody = document.getElementById("readmodeBody");
 
 async function readmodeThis(el) {
-  readmodeBody.innerHTML = `
-  <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
-    <p class="text-secondary small mb-2">Loading your article...</p>
-    <img class="svg-icon" src="./img/spinner.svg" width="24" height="24" alt="loading spinner" />
-  </div>
-  `;
   let readmodeModal = document.getElementById("readmodeModal");
   let modal = new bootstrap.Modal(readmodeModal);
   modal.show();
@@ -18,27 +12,23 @@ async function readmodeThis(el) {
   let reader = new Readability(doc);
   let article = reader.parse();
   console.log(article);
-  if (article && article.textContent === "Content not availableSorry, it was not possible to extract content from this website.Return") {
+  if (article.textContent === "Content not availableSorry, it was not possible to extract content from this website.Return") {
     let response = await fetch("https://web-production-09ad.up.railway.app/" + link);
     let html = await response.text();
     let doc = new DOMParser().parseFromString(html, "text/html");
     let reader = new Readability(doc);
     let article = reader.parse();
     console.log(article);
-    if (article && article.title) {
-      article.title = article.title.replace(/\(via:.*?\)/g, "").trim();
-    }
+    article.title = article.title.replace(/\(via:.*?\)/g, "").trim();
     readmodeBody.innerHTML = `
-    <h1 id="readmodeArticleTitle" class="fw-bold mb-2">${article ? article.title : "No title available"}</h1>
-    <div id="readmodeArticleBody">${article ? article.content : "No content available"}</div>
+    <h1 id="readmodeArticleTitle" class="fw-bold mb-2">${article.title}</h1>
+    <div id="readmodeArticleBody">${article.content}</div>
     `;
   } else {
-    if (article && article.title) {
-      article.title = article.title.replace(/\(via:.*?\)/g, "").trim();
-    }
+    article.title = article.title.replace(/\(via:.*?\)/g, "").trim();
     readmodeBody.innerHTML = `
-    <h1 id="readmodeArticleTitle" class="fw-bold mb-2">${article ? article.title : "No title available"}</h1>
-    <div id="readmodeArticleBody">${article ? article.content : "No content available"}</div>
+    <h1 id="readmodeArticleTitle" class="fw-bold mb-2">${article.title}</h1>
+    <div id="readmodeArticleBody">${article.content}</div>
     `;
   }
 }
